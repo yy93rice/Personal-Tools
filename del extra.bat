51ca9394452b3329fe02c07afe2ce2a8f866af86
yy93rice <yy93@rice.edu>
2021-11-06 22:40:50 -0500

    11/6/21

---
+++ del extra.bat
@@ -0,0 +1,5 @@
+del "C:\Users\Yifei Yang\Downloads\pdf&docs\*(?).docx"
+del "C:\Users\Yifei Yang\Downloads\pdf&docs\*(?).pdf"
+del "C:\Users\Yifei Yang\Downloads\pdf&docs\*(?).doc"
+
+del "C:\Users\Yifei Yang\Downloads\*(?).*"
---
+++ download cleanup.bat
@@ -0,0 +1,9 @@
+move "C:\Users\Yifei Yang\Downloads\*.ris" "C:\Users\Yifei Yang\Downloads\ref_files"
+move "C:\Users\Yifei Yang\Downloads\*.nbib" "C:\Users\Yifei Yang\Downloads\ref_files"
+
+del "C:\Users\Yifei Yang\Downloads\ref_files\*(?).ris"
+del "C:\Users\Yifei Yang\Downloads\ref_files\*(?).nbib"
+
+move "C:\Users\Yifei Yang\Downloads\*.pdf" "C:\Users\Yifei Yang\Downloads\pdf&docs"
+move "C:\Users\Yifei Yang\Downloads\*.docx" "C:\Users\Yifei Yang\Downloads\pdf&docs"
+move "C:\Users\Yifei Yang\Downloads\*.doc" "C:\Users\Yifei Yang\Downloads\pdb&docs"

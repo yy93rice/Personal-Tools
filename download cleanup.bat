72ea61c1b030783c7823ea9bcb58a8a7ac04a3a6
yy93rice <yy93@rice.edu>
2021-12-11 23:10:45 -0600

    Update download cleanup.bat

--- download cleanup.bat
+++ download cleanup.bat
@@ -1,10 +1,14 @@
-move "C:\Users\Yifei Yang\Downloads\*.ris" "C:\Users\Yifei Yang\Downloads\ref_files"
-move "C:\Users\Yifei Yang\Downloads\*.nbib" "C:\Users\Yifei Yang\Downloads\ref_files"
+if not exist "C:\Users\%USERNAME%\Downloads\ref_files" mkdir "C:\Users\%USERNAME%\Downloads\ref_files"
+if not exist "C:\Users\%USERNAME%\Downloads\pdf&docs" mkdir "C:\Users\%USERNAME%\Downloads\pdf&docs"
 
-move "C:\Users\Yifei Yang\Downloads\*.pdf" "C:\Users\Yifei Yang\Downloads\pdf&docs"
-move "C:\Users\Yifei Yang\Downloads\*.docx" "C:\Users\Yifei Yang\Downloads\pdf&docs"
-move "C:\Users\Yifei Yang\Downloads\*.doc" "C:\Users\Yifei Yang\Downloads\pdb&docs"
 
-del "C:\Users\Yifei Yang\Downloads\pdf&docs\*(?).*"
-del "C:\Users\Yifei Yang\Downloads\ref_files\*(?).*"
-del "C:\Users\Yifei Yang\Downloads\*(?).*"
+move "C:\Users\%USERNAME%\Downloads\*.ris" "C:\Users\%USERNAME%\Downloads\ref_files"
+move "C:\Users\%USERNAME%\Downloads\*.nbib" "C:\Users\%USERNAME%\Downloads\ref_files"
+
+move "C:\Users\%USERNAME%\Downloads\*.pdf" "C:\Users\%USERNAME%\Downloads\pdf&docs"
+move "C:\Users\%USERNAME%\Downloads\*.docx" "C:\Users\%USERNAME%\Downloads\pdf&docs"
+move "C:\Users\%USERNAME%\Downloads\*.doc" "C:\Users\%USERNAME%\Downloads\pdb&docs"
+
+del "C:\Users\%USERNAME%\Downloads\pdf&docs\*(?).*"
+del "C:\Users\%USERNAME%\Downloads\ref_files\*(?).*"
+del "C:\Users\%USERNAME%\Downloads\*(?).*"
